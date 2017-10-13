5b089179baa244c2e92a1626321ba763a6d41734
johanliu <akira.lau@hotmail.com>
2017-10-13 18:39:08 +0800

    implement the logic, error handler

--- dns.go
+++ dns.go
@@ -4,50 +4,58 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
+	"net/http"
+	"strings"
+	"time"
 
+	"github.com/coreos/etcd/client"
 	"github.com/johanliu/essos"
 	"github.com/johanliu/essos/components"
+	"github.com/johanliu/essos/interfaces"
+	"github.com/johanliu/vidar/constant"
 )
 
 const (
-	VERSION = "1.0"
-
-	ADD = iota
-	DELETE
-	UPDATE
-	READ
-
-	API_CONTENT_HEADER  = "application/json;charset=utf-8"
-	ETCD_CONTENT_HEADER = "application/x-www-form-urlencoded"
-
-	ADD_METHOD     = "PUT"
-	DELETE_METHOD  = "DELETE"
-	CONTENT_HEADER = "Content-Type"
-
-	NORVOS_PATH             = "/norvos"
-	DEFAULT_ETCD_NORVOS_LOC = "/v2/keys/norvos/node/purge/"
-	DEFAULT_ETCD_PORT       = "2379"
-	DEFAULT_ETCD_TIMEOUT    = 5
-	DEFAULT_SKYDNS_LOC      = "/v2/keys/skydns/"
-	DEFAULT_ARPA_LOC        = "/v2/keys/skydns/arpa/in-addr/"
-	DEFAULT_SCAN_ARGS       = "/?recursive=true"
-	DEFAULT_ETCD_CAS        = "?prevValue="
-	DEFAULT_TTL             = 60
-
-	DEFAULT_TRIM_KEY        = "/skydns/"
-	DEFAULT_TRIM_ARPA_KEY   = DEFAULT_TRIM_KEY + "arpa/in-addr/"
-	DEFAULT_TRIM_SERVER_KEY = "/norvos/node/purge/"
-
-	DEFAULT_TIMEOUT = 5
+	version    = "1.0"
+	root       = "/coredns/"
+	defaultTTL = 300
 )
+
+var cli client.Client
 
 type norvos struct {
 	ops map[string]essos.Operation
 }
 
+type Message struct {
+	result string
+}
+
 func (n *norvos) Discover() map[string]essos.Operation {
 	return n.ops
+}
+
+func (n *norvos) Start(params interface{}) error {
+	var err error
+	p, ok := params.(interfaces.DNS)
+	if !ok {
+		return errors.New("Params format is mismatched with pre-defined one")
+	}
+
+	cfg := client.Config{
+		Endpoints:               []string{p.Etcd},
+		Transport:               client.DefaultTransport,
+		HeaderTimeoutPerRequest: time.Second,
+	}
+	cli, err = client.New(cfg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (n *norvos) Stop() error {
+	return nil
 }
 
 func init() {
@@ -56,90 +64,287 @@ func init() {
 			ops: map[string]essos.Operation{
 				"create": create("Create"),
 				"read":   read("Read"),
-				"delete": delete("Delete"),
 				"update": update("Update"),
+				"delete": delete("Delete"),
 			},
 		})
+}
+
+func reverse(s []string) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
 }
 
 type create string
 
 func (create) Description() string {
-	return "This is Create"
-}
-
-type Group struct {
-	Name   string
-	Colors []string
+	return "Create operation for component of dns"
 }
 
 func (create) Do(ctx context.Context, args []string) (context.Context, error) {
+	type payload struct {
+		Host        string `json:"host"`
+		Port        int    `json:"port"`
+		Priority    int    `json:"priority"`
+		Weight      int    `json:"weight"`
+		TTL         int    `json:"ttl"`
+		TargetStrip int    `json:"targetstrip"`
+	}
+
+	type request struct {
+		Name string
+		payload
+	}
+
+	type response struct {
+		Message *client.Response `json:"message"`
+	}
+
 	if ctx == nil {
 		return ctx, errors.New("context is nil")
 	}
 
-	group := Group{
-		Name:   "Reds",
-		Colors: []string{"A", "B"},
+	input := ctx.Value("input").([]byte)
+	req := new(request)
+	if err := json.Unmarshal(input, req); err != nil {
+		return nil, err
 	}
 
-	message, err := json.Marshal(group)
+	if req.TTL == 0 {
+		req.TTL = defaultTTL
+	}
+
+	api := client.NewKeysAPI(cli)
+
+	name := strings.Split(req.Name, ".")
+	reverse(name)
+	req.Name = strings.Join(name, "/")
+
+	key := root + req.Name
+	value, err := json.Marshal(req.payload)
 	if err != nil {
-		fmt.Println("wtf")
+		return nil, err
 	}
 
-	result := essos.Response{
-		Code:    200,
-		Message: message,
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	resp, err := api.Create(ctx, key, string(value))
+	if err != nil {
+		ce, ok := err.(client.Error)
+		if ok {
+			if ce.Code == client.ErrorCodeNodeExist {
+				return nil, constant.NewHTTPError(http.StatusAlreadyReported, ce.Message)
+			} else {
+				return nil, constant.NewHTTPError(http.StatusBadGateway, ce.Message)
+			}
+		} else if err == context.DeadlineExceeded {
+			return nil, constant.GatewayTimeoutError
+		} else {
+			return nil, err
+		}
+	}
+
+	result := response{
+		Message: resp,
 	}
 
 	ctx = context.WithValue(ctx, "result", result)
-
 	return ctx, nil
 }
 
 type delete string
 
 func (delete) Description() string {
-	return "This is Delete"
+	return "Delete operation for component of dns"
 }
 
 func (delete) Do(ctx context.Context, args []string) (context.Context, error) {
+	type request struct {
+		Name string
+	}
+
+	type response struct {
+		Message *client.Response `json:"message"`
+	}
+
 	if ctx == nil {
 		return ctx, errors.New("context is nil")
 	}
 
+	input := ctx.Value("input").([]byte)
+	req := new(request)
+	if err := json.Unmarshal(input, req); err != nil {
+		return nil, err
+	}
+
+	api := client.NewKeysAPI(cli)
+
+	name := strings.Split(req.Name, ".")
+	reverse(name)
+	req.Name = strings.Join(name, "/")
+
+	key := root + req.Name
+
+	resp, err := api.Delete(context.Background(), key, &client.DeleteOptions{Recursive: false})
+	if err != nil {
+		ce, ok := err.(client.Error)
+		if ok {
+			if ce.Code == client.ErrorCodeKeyNotFound {
+				return nil, constant.NewHTTPError(http.StatusNoContent, ce.Message)
+			} else {
+				return nil, constant.NewHTTPError(http.StatusBadGateway, ce.Message)
+			}
+		} else if err == context.DeadlineExceeded {
+			return nil, constant.GatewayTimeoutError
+		} else {
+			return nil, err
+		}
+	}
+
+	result := response{
+		Message: resp,
+	}
+
+	ctx = context.WithValue(ctx, "result", result)
 	return ctx, nil
 }
 
 type update string
 
 func (update) Description() string {
-	return "This is Update"
+	return "Update operation for component of dns. Sorry, no Upsert"
 }
 
 func (update) Do(ctx context.Context, args []string) (context.Context, error) {
+	type payload struct {
+		Host        string `json:"host"`
+		Port        int    `json:"port"`
+		Priority    int    `json:"priority"`
+		Weight      int    `json:"weight"`
+		TTL         int    `json:"ttl"`
+		TargetStrip int    `json:"targetstrip"`
+	}
+
+	type request struct {
+		Name string
+		payload
+	}
+
+	type response struct {
+		Message *client.Response `json:"message"`
+	}
+
 	if ctx == nil {
 		return ctx, errors.New("context is nil")
 	}
 
+	input := ctx.Value("input").([]byte)
+	req := new(request)
+	if err := json.Unmarshal(input, req); err != nil {
+		return nil, err
+	}
+
+	if req.TTL == 0 {
+		req.TTL = defaultTTL
+	}
+
+	api := client.NewKeysAPI(cli)
+
+	name := strings.Split(req.Name, ".")
+	reverse(name)
+	req.Name = strings.Join(name, "/")
+
+	key := root + req.Name
+	value, err := json.Marshal(req.payload)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	resp, err := api.Update(ctx, key, string(value))
+	if err != nil {
+		ce, ok := err.(client.Error)
+		if ok {
+			if ce.Code == client.ErrorCodeKeyNotFound {
+				return nil, constant.NewHTTPError(http.StatusNoContent, ce.Message)
+			} else {
+				return nil, constant.NewHTTPError(http.StatusBadGateway, ce.Message)
+			}
+		} else if err == context.DeadlineExceeded {
+			return nil, constant.GatewayTimeoutError
+		} else {
+			return nil, err
+		}
+	}
+
+	result := response{
+		Message: resp,
+	}
+
+	ctx = context.WithValue(ctx, "result", result)
 	return ctx, nil
 }
 
 type read string
 
 func (read) Description() string {
-	return "This is Read"
+	return "Read operation for component of dns"
 }
 
 func (read) Do(ctx context.Context, args []string) (context.Context, error) {
+	type request struct {
+		Name string
+	}
+
+	type response struct {
+		Message *client.Response `json:"message"`
+	}
+
 	if ctx == nil {
 		return ctx, errors.New("context is nil")
 	}
 
-	return ctx, nil
-}
+	input := ctx.Value("input").([]byte)
+	req := new(request)
+	if err := json.Unmarshal(input, req); err != nil {
+		return nil, err
+	}
 
-func do(args []string, data interface{}) error {
-	return nil
+	api := client.NewKeysAPI(cli)
+
+	name := strings.Split(req.Name, ".")
+	reverse(name)
+	req.Name = strings.Join(name, "/")
+
+	key := root + req.Name
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	resp, err := api.Get(ctx, key, &client.GetOptions{Quorum: true})
+	if err != nil {
+		ce, ok := err.(client.Error)
+		if ok {
+			if ce.Code == client.ErrorCodeKeyNotFound {
+				return nil, constant.NewHTTPError(http.StatusNoContent, ce.Message)
+			} else {
+				return nil, constant.NewHTTPError(http.StatusBadGateway, ce.Message)
+			}
+		} else if err == context.DeadlineExceeded {
+			return nil, constant.GatewayTimeoutError
+		} else {
+			return nil, err
+		}
+	}
+
+	result := response{
+		Message: resp,
+	}
+
+	ctx = context.WithValue(ctx, "result", result)
+	return ctx, nil
 }
